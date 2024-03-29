1140b2740d291c1771fba3532a034266accfc99a
ttddww <woldeteketel@gmail.com>
2024-03-28 22:25:26 -0400

    initial commit

--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -9,7 +9,7 @@ const authMiddleware = require("../middlewares/auth.middleware");
 // Create a route to handle the add employee request on post
 router.post(
   "/api/employee",
-  [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
   employeeController.createEmployee
 );
 // Create a route to handle the get all employees request on get
