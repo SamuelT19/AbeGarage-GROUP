c95c6e85c6171e47aa751c6d6b84033cc3f71dbe
chalewkg <chalewkg@gmail.com>
2024-03-31 22:24:52 -0400

    custome edit, get bi id, get all customer and deleted added

--- backend/services/customer.service.js
+++ backend/services/customer.service.js
@@ -52,4 +52,60 @@ async function addCustomer(customer) {
   }
 }
 
-module.exports = { addCustomer, checkIfCustomerExists };
+// get all customers
+async function getAllCustomers() {
+  const query =
+    "SELECT * FROM customer_info JOIN customer_identifier ON customer_info.customer_id = customer_identifier.customer_id";
+
+  const rows = await conn.query(query);
+  return rows;
+}
+
+// get customer by id
+async function getCustomerById(customerId) {
+  const query = `
+    SELECT customer_info.*, customer_identifier.customer_email, customer_identifier.customer_phone_number, customer_identifier.customer_added_date, customer_identifier.customer_hash
+    FROM customer_info
+
+    LEFT JOIN customer_identifier ON customer_info.customer_id = customer_identifier.customer_id
+    WHERE customer_info.customer_id = ?
+  `;
+  const rows = await conn.query(query, [customerId]);
+  return rows;
+}
+
+// delete customer by id
+async function deleteCustomerById(customerId) {
+  // delete from customer_info
+  const deleteCustomerInfo = "DELETE FROM customer_info WHERE customer_id = ?";
+  await conn.query(deleteCustomerInfo, [customerId]);
+
+  const deleteCustomerIdentifier =
+    "DELETE FROM customer_identifier WHERE customer_id = ?";
+  await conn.query(deleteCustomerIdentifier, [customerId]);
+
+  return true;
+}
+
+// edit customer
+
+async function editCustomer(updatedCustomerData) {
+  const query =
+    "UPDATE customer_info SET customer_first_name = ?, customer_last_name = ?, active_customer_status = ? WHERE customer_id = ?";
+  const rows = await conn.query(query, [
+    updatedCustomerData.customer_first_name,
+    updatedCustomerData.customer_last_name,
+    updatedCustomerData.active_customer_status,
+    updatedCustomerData.customer_id,
+  ]);
+  return rows;
+}
+
+module.exports = {
+  addCustomer,
+  checkIfCustomerExists,
+  getAllCustomers,
+  getCustomerById,
+  deleteCustomerById,
+  editCustomer,
+};
