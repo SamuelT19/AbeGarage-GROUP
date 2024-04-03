6a3e8accd27ecd06f0075cab26e690928973d041
true <trueworku@gmail.com>
2024-04-03 17:24:12 -0400

    delete-vehicle backend

--- backend/routes/customer.routes.js
+++ backend/routes/customer.routes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const customerController = require("../controllers/customer.controller");
-// const authMiddleware = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 // Create a route to handle the add customer request on post
 router.post(
@@ -13,12 +13,12 @@ router.post(
 
 // Create a route to handle the get all customers request on get
 router.get("/api/customers",
-  [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  // [authMiddleware.verifyToken, authMiddleware.isAdmin],
   customerController.getAllCustomers
 );
 // Create a route to handle the get customer by ID request on get
 router.get("/api/customer/:customer_id",
-  [authMiddleware.verifyToken, authMiddleware.isAdmin],
+   [authMiddleware.verifyToken, authMiddleware.isAdmin],
   customerController.getCustomerById
 );
 // Create a route to handle the edit customer request on post
@@ -31,7 +31,6 @@ router.delete( "/api/customer/:customer_id",
   [authMiddleware.verifyToken, authMiddleware.isAdmin],
   customerController.deleteCustomerById
 );
-
 
 // Export the router
 module.exports = router;
