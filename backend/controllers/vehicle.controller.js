6214fbebc4afa3634fb39ccfbc6c1b2f9677cbd0
ttddww <woldeteketel@gmail.com>
2024-04-04 23:37:15 -0400

    update

--- backend/controllers/vehicle.controller.js
+++ backend/controllers/vehicle.controller.js
@@ -84,11 +84,38 @@ async function deleteVehicle(req, res, next) {
   }
 }
 
-// Export the delete vehicle controller
 
-module.exports = { 
-    createVehicle, 
-    deleteVehicle,
-     getVehicleById 
-    };
+// Create the get all vehicles controller
+async function getAllVehicles(req, res, next) {
+    const customerId = req.params.customer_id;
+  try {
+    // Call the getAllVehicles method from the vehicle service
+    const vehicles = await vehicleService.getAllVehicles(customerId);
+    if (!vehicles) {
+      return res.status(400).json({
+        error: "Failed to get all vehicles!",
+      });
+    }
+
+    // Success response
+    res.status(200).json({
+      status: "success",
+      data: vehicles,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      error: "Something went wrong!",
+    });
+  }
+}
+
+
+
+module.exports = {
+  createVehicle,
+  deleteVehicle,
+  getVehicleById,
+  getAllVehicles,
+};
 
--- backend/routes/vehicle.routes.js
+++ backend/routes/vehicle.routes.js
@@ -25,4 +25,9 @@ router.delete(
   [authMiddleware.verifyToken, authMiddleware.isAdmin],
   vehicleController.deleteVehicle
 );
+
+//  create a route to get all vehicles by customer
+
+router.get ("/api/all-vehicles/:customer_id",  [authMiddleware.verifyToken, authMiddleware.isAdmin], vehicleController.getAllVehicles);
+
 module.exports = router;
