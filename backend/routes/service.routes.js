472af853ae1eadecff245082218678c38d2745dc
SamuelT19 <139996430+SamuelT19@users.noreply.github.com>
2024-04-05 22:50:02 +0300

    Delete backend/routes/service.routes.js

--- backend/routes/service.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-// Call the router method from express to create the router
-const router = express.Router();
-
-const commonServiceController = require('../controllers/service.controller')
-
-router.post("/api/service/add", commonServiceController.addService);
-
-module.exports = router;
