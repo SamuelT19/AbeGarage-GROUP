108be7e5fba33971648218e7ded342661a5c2780
chalewkg <chalewkg@gmail.com>
2024-04-14 20:57:36 -0400

    updated edit function

--- backend/services/order.service.js
+++ backend/services/order.service.js
@@ -1,84 +1,6 @@
 const { query, withTransaction } = require("../config/db.config");
 const { v4: uuidv4 } = require("uuid");
 
-// //createOrder service
-// const createOrder = async (orderData) => {
-//   try {
-//     await withTransaction(async () => {
-//       const order_hash = uuidv4();
-
-//       const query1 =
-//         "INSERT INTO orders (employee_id, customer_id, vehicle_id, active_order, order_hash) VALUES (?,?,?,?,?)";
-//       const rows1 = await query(query1, [
-//         orderData.employee_id,
-//         orderData.customer_id,
-//         orderData.vehicle_id,
-//         orderData.active_order,
-//         order_hash,
-//       ]);
-//       if (rows1.affectedRows !== 1) {
-//         return false;
-//       }
-
-//       const order_id = rows1.insertId;
-
-//       // Insert into the order_services table
-//       if (orderData.order_services && orderData.order_services.length > 0) {
-//         const orderServicesValues = orderData.order_services
-//           .map(
-//             (service) =>
-//               `(${order_id}, ${service.service_id}, ${service.service_completed})`
-//           )
-//           .join(",");
-//         const orderServicesQuery = `INSERT INTO order_services (order_id, service_id, service_completed) VALUES ${orderServicesValues}`;
-//         const orderServicesRows = await query(orderServicesQuery);
-
-//         if (
-//           orderServicesRows.affectedRows !== orderData.order_services.length
-//         ) {
-//           console.log("Failed to insert into order_services table");
-//           return false;
-//         }
-//       }
-
-//       //insert into the order_info table
-//       const query4 =
-//         "INSERT INTO order_info (order_id, order_total_price, estimated_completion_date, completion_date, additional_request, notes_for_internal_use, notes_for_customer, additional_requests_completed) VALUES (?,?,?,?,?,?,?,?) ";
-//       const rows4 = await query(query4, [
-//         order_id,
-//         orderData.order_total_price,
-//         orderData.estimated_completion_date,
-//         orderData.completion_date,
-//         orderData.additional_request,
-//         orderData.notes_for_internal_use,
-//         orderData.notes_for_customer,
-//         orderData.additional_requests_completed,
-//       ]);
-
-//       // Insert into the order_status table
-//       if (orderData.order_status) {
-//         const orderStatusQuery =
-//           "INSERT INTO order_status (order_id, order_status) VALUES (?, ?)";
-//         const orderStatusRows = await query(orderStatusQuery, [
-//           order_id,
-//           orderData.order_status,
-//         ]);
-
-//         if (orderStatusRows.affectedRows !== 1) {
-//           console.log("Failed to insert into order_status table");
-//           return false;
-//         }
-//       } else {
-//         console.log("Invalid order_status value");
-//         return false;
-//       }
-//     });
-//     return true;
-//   } catch (error) {
-//     console.log("Transaction error:", error);
-//   }
-// };
-
 // with suucess validation
 const createOrder = async (orderData) => {
   try {
@@ -245,8 +167,8 @@ async function getAllOrders() {
 
 //edit order service
 const editOrder = async (orderData) => {
-  let success = false;
   try {
+    let success = false;
     await withTransaction(async (connection) => {
       const order_id = orderData.order_id;
       console.log(order_id);
@@ -292,7 +214,7 @@ additional_request = ?, additional_requests_completed = ? WHERE order_id = ?`;
       // Update order_services table
       if (orderData.order_services && orderData.order_services.length > 0) {
         for (const service of orderData.order_services) {
-          const updateServiceQuery = ` UPDATE order_services SET service_completed = ? WHERE order_id = ? AND service_id = ?`;
+          const updateServiceQuery = `UPDATE order_services SET service_completed = ? WHERE order_id = ? AND service_id = ?`;
           const rows4 = await connection.execute(updateServiceQuery, [
             service.service_completed,
             order_id,
@@ -300,7 +222,7 @@ additional_request = ?, additional_requests_completed = ? WHERE order_id = ?`;
           ]);
 
           console.log(rows4);
-          if (rows4.affectedRows !== orderData.order_services.length) {
+          if (!rows4.affectedRows) {
             throw new Error("Failed to update order_services table");
           }
         }
@@ -320,83 +242,3 @@ module.exports = {
   getAllOrders,
   editOrder,
 };
-
-/* edite trails
-// const editOrder = async (orderData) => {
-//   let transactionSuccess = false;
-//   try {
-//     await withTransaction(async (connection) => {
-//       // Passing the connection parameter
-//       const order_id = orderData.order_id;
-
-//       const updateOrderQuery = `UPDATE orders SET active_order = ? WHERE order_id = ?`;
-//       const updateOrderInfoQuery = `UPDATE order_info SET order_total_price = ?, estimated_completion_date = ?, completion_date = ?, additional_request = ?, additional_requests_completed = ? WHERE order_id = ?`;
-//       const updateOrderStatusQuery = `UPDATE order_status SET order_status = ? WHERE order_id = ?`;
-//       const updateOrderServicesQuery = `UPDATE order_services SET service_completed = ? WHERE order_id = ? AND service_id = ?`;
-
-//       const queries = [
-//         {
-//           query: updateOrderQuery,
-//           values: [orderData.active_order, order_id],
-//         },
-//         {
-//           query: updateOrderInfoQuery,
-//           values: [
-//             orderData.order_total_price,
-//             orderData.estimated_completion_date,
-//             orderData.completion_date,
-//             orderData.additional_request,
-//             orderData.additional_requests_completed,
-//             order_id,
-//           ],
-//         },
-//         {
-//           query: updateOrderStatusQuery,
-//           values: [orderData.order_status, order_id],
-//         },
-//       ];
-
-//       if (orderData.order_services && orderData.order_services.length > 0) {
-//         for (const service of orderData.order_services) {
-//           queries.push({
-//             query: updateOrderServicesQuery,
-//             values: [service.service_completed, order_id, service.service_id],
-//           });
-//         }
-//       }
-
-//       // Execute all update queries in the transaction
-//       for (const { query, values } of queries) {
-//         const [result] = await connection.query(query, values); // Changed from query(query, values) to connection.query(query, values)
-//         if (result.affectedRows !== 1) {
-//           console.error(`Failed to execute query: ${query}`);
-//           throw new Error(
-//             "Transaction failed: query did not affect the expected number of rows"
-//           );
-//         }
-//       }
-
-//       transactionSuccess = true;
-//     });
-//   } catch (error) {
-//     console.error("Transaction error:", error);
-//     // Rollback transaction here if necessary
-//   } finally {
-//     if (!transactionSuccess) {
-//       console.log("Rolling back transaction...");
-//       try {
-//         await withTransaction(async (connection) => {
-//           await connection.rollback(); // Rollback the transaction
-//           console.log("Transaction rolled back successfully.");
-//         });
-//       } catch (rollbackError) {
-//         console.error("Rollback failed:", rollbackError);
-//         throw rollbackError; // Re-throw the error to handle it at a higher level
-//       }
-//     }
-//   }
-// };
-
-
-
-*/
