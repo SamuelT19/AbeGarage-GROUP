92319592981264f185364352adfd348ba02aa717
chalewkg <chalewkg@gmail.com>
2024-06-07 19:14:53 -0400

    commneted code removed

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -61,32 +61,6 @@ async function editOrder(req, res, next) {
   }
 }
 
-// const orderedServices = async (req, res) => {
-//   try {
-//     const order_id = req.params.order_id;
-//     console.log(order_id);
-
-//     const services = await orderService.orderedServices(order_id);
-
-//     const [customerVehicle] = await orderService.getVehicleByOrderId(order_id);
-
-//     if (!services) {
-//       res.status(400).json({
-//         error: "Failed to fetch order",
-//       });
-//     } else {
-//       res.status(200).json({
-//         message: "single order fetched successfully",
-//         services,
-//         customerVehicle,
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return error;
-//   }
-// };
-
 const singleOrder = async (req, res) => {
   try {
     const { order_id } = req.params;
